3954ee293d04e56fa944c40869255a24d81551bf
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-08-31 23:12:54 +0900

    flush

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io::{stdout, Write, BufWriter};
+//use std::{thread, time};
 fn main() {
     println!("Hello, world!");
     println!("Hollo, world!");
-    println!("{}",0x7f);
     let out = stdout();
     let mut out = BufWriter::new(out.lock());
+    //let ten_millis = time::Duration::from_millis(100);
     for _ in 0..10 {
+        out.write(b"\x1B[2J").unwrap();
         out.write(b"yes\n").unwrap();
-        out.write(0x7f).unwrap();
+        //thread::sleep(ten_millis);
     }
 }
