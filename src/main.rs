f5ea99f38b3affb7a7624eaee87de55ae97724fd
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-08-31 10:26:11 +0900

    screen test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{stdout, Write, BufWriter};
 fn main() {
     println!("Hello, world!");
+    println!("Hollo, world!");
+    println!("{}",0x7f);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    for _ in 0..10 {
+        out.write(b"yes\n").unwrap();
+    }
 }
